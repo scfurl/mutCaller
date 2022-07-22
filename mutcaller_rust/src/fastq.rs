623b2eb5831773c8b6d54ce2cd6839edbae0a842
Scott Furlan <scfurl@users.noreply.github.com>
2022-07-22 10:09:24 -0700

    mostlyworking

--- mutcaller_rust/src/fastq.rs
+++ mutcaller_rust/src/fastq.rs
@@ -1,28 +1,29 @@
-/**
+/*
 
-chmod 777 /Users/sfurlan/develop/mutCaller/mutcaller_rust/target/debug/mutcaller_rust
-cd /Users/sfurlan/develop/mutCaller/mutcaller_rust
-cargo build
-/Users/sfurlan/develop/mutCaller/mutcaller_rust/target/debug/mutcaller_rust -t 18 --ibam "/Users/sfurlan/Fred Hutchinson Cancer Research Center/Furlan_Lab - General/experiments/patient_marrows/LKmut/Aligned.out.tagged.sorted_0.05.bam"
+cd ~/develop/mutCaller/mutcaller_rust
+/Users/sfurlan/develop/mutCaller/mutcaller_rust/target/debug/fastq3 --ifastq "/Users/sfurlan/Fred Hutchinson Cancer Research Center/Furlan_Lab - General/experiments/patient_marrows/LKmut/sample_filtered.fastq_1M.fastq.gz"
+*/
 
-/Users/sfurlan/develop/mutCaller/mutcaller_rust/target/debug/fastq -t 8 --ifastq "/Users/sfurlan/Fred Hutchinson Cancer Research Center/Furlan_Lab - General/experiments/patient_marrows/LKmut/sample_filtered.fastq_1M.fastq.gz"
-
-**/
-
-
-
+/*
+### TO DO ###
+add option for running in debug mode
+measure speed
+*/
 
 extern crate clap;
 extern crate fastq;
 extern crate parasailors;
 
+use bytes::BytesMut;
 use std::sync::{Arc, Mutex};
-use fastq::{parse_path, Record};
-// use std::env::args;
+use fastq::{parse_path, Record, RefRecord};
 use parasailors as align;
 use clap::{App, load_yaml};
 use std::io::{self, Write};
 use std::io::stdout;
+use std::vec::Vec;
+
+
 
 #[derive(Clone)]
 struct Params {
@@ -55,43 +56,101 @@ fn main() {
     fastq(&params);
 }
 
+
 fn fastq(params: &Params) {
+    let mut total: usize = 0;
+    let split_at: u8 = 116 - (params.umi_len + params.cb_len);
     let filename: Option<String> = Some(params.ifastq.to_string());
     // Treat "-" as stdin
+
     let path = match filename.as_ref().map(String::as_ref) {
         None | Some("-") => { None },
         Some(name) => Some(name)
     };
-
-
-    parse_path(path, |parser| {
-        let nthreads = params.threads as usize;
-        let results: Vec<usize> = parser.parallel_each(nthreads, |record_sets| {
-            // we can initialize thread local variables here.
-            let adapter = b"AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT";
-            let matrix = align::Matrix::new(align::MatrixType::Identity);
-            let profile = align::Profile::new(adapter, &matrix);
-            let mut thread_total = 0;
-            // let handle = std::io::stdout();
-            // let buf = Mutex::new(BufWriter::new(handle));
-            let mut writer = io::stdout(); // get the global stdout entity
-            // let mut handle = io::BufWriter::new(writer); 
-            for record_set in record_sets {
-                for record in record_set.iter() {
-                    let score = align::local_alignment_score(&profile, record.seq(), 5, 1);
-                    if score > 8 {
-                        thread_total += 1;
-                    }
-                    record.write(& mut writer);
-                }
-            }
-
-            // The values we return (it can be any type implementing `Send`)
-            // are collected from the different threads by
-            // `parser.parallel_each` and returned. See doc for a description of
-            // error handling.
-            thread_total
+    parse_path(path, |mut parser| {
+        
+        let stopped = parser.each(|record| {
+            // stop parsing if we find a sequnce containing 'N'
+            record.validate_dnan();
+            modify_fastq(record, &mut total, split_at)
         }).expect("Invalid fastq file");
-        println!("{}", results.iter().sum::<usize>());
+        if stopped {
+            println!("The file contains only sequences with ACTGN");
+        } else {
+            println!("The file contains invalid sequences");
+        }
     }).expect("Invalid compression");
 }
+
+
+
+fn modify_fastq (record: RefRecord, total: &mut usize, split_at: u8) -> bool {
+    let mut writer = io::stdout();
+    let mut owned_rec = RefRecord::to_owned_record(&record);
+    let mut curr_bytes = BytesMut::from(owned_rec.qual());
+    let (seq, barcode) = &curr_bytes.split_at(split_at.into());
+    owned_rec.qual = seq.to_vec();
+    let mut curr_bytes = BytesMut::from(owned_rec.seq());
+    let (seq, mut barcode) = &curr_bytes.split_at(split_at.into());
+    owned_rec.seq = seq.to_vec();
+    let mut curr_bytes = BytesMut::from(owned_rec.head()).to_vec();
+    let _ = &curr_bytes.push(':' as u8);
+    let _ = &curr_bytes.append(&mut barcode.to_vec());
+    owned_rec.head = curr_bytes;
+    owned_rec.sep = Some(vec!['+' as u8]);
+    owned_rec.write(&mut writer);
+    *total += 1;
+    true
+}
+
+fn modify_fastq_debug(record: RefRecord, total: &mut usize, split_at: u8) -> bool {
+    println!("Before mod");
+    println!("{}", String::from_utf8_lossy(record.head()));
+    println!("{}", String::from_utf8_lossy(record.seq()));
+    println!("{}", String::from_utf8_lossy(record.qual()));
+    let mut owned_rec = RefRecord::to_owned_record(&record);
+    let mut curr_bytes = BytesMut::from(owned_rec.qual());
+    let (seq, barcode) = &curr_bytes.split_at(split_at.into());
+    owned_rec.qual = seq.to_vec();
+    let mut curr_bytes = BytesMut::from(owned_rec.seq());
+    let (seq, mut barcode) = &curr_bytes.split_at(split_at.into());
+    owned_rec.seq = seq.to_vec();
+    let mut curr_bytes = BytesMut::from(owned_rec.head()).to_vec();
+    let _ = &curr_bytes.push(':' as u8);
+    let _ = &curr_bytes.append(&mut barcode.to_vec());
+    owned_rec.head = curr_bytes;
+    owned_rec.sep = Some(vec!['+' as u8]);
+    // owned_rec.head = &curr_bytes.append(&mut barcode.to_vec());
+    // owned_rec.head = curr_bytes.into_iter().chain(barcode.into_iter()).collect();
+    // owned_rec.head = curr_bytes.to_vec();
+    println!("After mod");
+    println!("{}", String::from_utf8_lossy(owned_rec.head()));
+    println!("{}", String::from_utf8_lossy(owned_rec.seq()));
+    println!("{}", String::from_utf8_lossy(owned_rec.qual()));
+    *total += 1;
+    true
+}
+
+
+
+
+// use bytes::BytesMut;
+// use fastq::{Parser, Record, RefRecord};
+
+// const READS: &str = r#"@read1/ENST00000266263.10;mate1:84-183;mate2:264-363
+// GACAGCCAGGGGCCAGCGGGTGGCAGTGCCCAGGACATAGAGAGAGGCAGCACACACGCGGTTGATGGTGAAGCCCGGAATGGCCACAGAGGCTAGAGCC
+// +
+// IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+// @read2/ENST00000266263.10;mate1:163-262;mate2:283-382
+// GATGCCATTGACAAAGGCAAGAAGGCTGGAGAGGTGCCCAGCCCTGAAGCAGGCCGCAGCGCCAGGGTGACTGTGGCTGTGGTGGACACCTTTGTATGGC
+// +
+// IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+// @read3/ENST00000266263.10;mate1:86-185;mate2:265-364
+// GGACAGCCAGGGGCCAGCGGGTGGCAGTGCCCAGGACATAGAGAGAGGCAGCANACACACGGTTGATGGTGAAGCCCGGAATGGCCACAGAGGCTAGAGC
+// +
+// IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+// @read4/ENST00000266263.10;mate1:297-396;mate2:401-500
+// CAGGAGGAGCTGGGCTTCCCCACTGTTAGGTAGAGCTTGCGCAGGCTGGAGTCCAGGAGGAAATCCACCGACCTGTCAATGGGGTGGATAATGATGGGGA
+// +
+// IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+// "#;
