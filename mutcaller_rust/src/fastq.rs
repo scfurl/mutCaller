6a6af39f1f8dbadad686cb00d1d49ec6ee8139da
Scott Furlan <scfurl@users.noreply.github.com>
2022-07-23 20:54:09 -0700

    workingwithlog

--- mutcaller_rust/src/fastq.rs
+++ mutcaller_rust/src/fastq.rs
@@ -1,41 +1,58 @@
 /*
 
 cd ~/develop/mutCaller/mutcaller_rust
-/Users/sfurlan/develop/mutCaller/mutcaller_rust/target/debug/fastq3 --ifastq "/Users/sfurlan/Fred Hutchinson Cancer Research Center/Furlan_Lab - General/experiments/patient_marrows/LKmut/sample_filtered.fastq_1M.fastq.gz"
+/Users/sfurlan/develop/mutCaller/mutcaller_rust/target/debug/fastq --ifastq "/Users/sfurlan/Fred Hutchinson Cancer Research Center/Furlan_Lab - General/experiments/patient_marrows/LKmut/sample_filtered.fastq_1M.fastq.gz" > test.fastq
+cat fastq.log
+tail test.fastq
+head test.fastq
+
+/Users/sfurlan/develop/mutCaller/mutcaller_rust/target/debug/fastq --ifastq "/Users/sfurlan/Fred Hutchinson Cancer Research Center/Furlan_Lab - General/experiments/patient_marrows/LKmut/sample_filtered.fastq_1M.fastq.gz"
+
 */
 
 /*
 ### TO DO ###
-add option for running in debug mode
 measure speed
 */
+
+
+#[macro_use]
+extern crate simple_log;
+
 
 extern crate clap;
 extern crate fastq;
 extern crate parasailors;
 
+use simple_log::LogConfigBuilder;
+
 use bytes::BytesMut;
-use std::sync::{Arc, Mutex};
+// use std::sync::{Arc, Mutex};
 use fastq::{parse_path, Record, RefRecord};
-use parasailors as align;
+// use parasailors as align;
 use clap::{App, load_yaml};
-use std::io::{self, Write};
-use std::io::stdout;
-use std::vec::Vec;
+use std::io::{self};
+// use std::io::stdout;
+// use std::vec::Vec;
 
-
-
-#[derive(Clone)]
+// #[derive(Clone)]
 struct Params {
     ifastq: String,
     umi_len: u8,
     cb_len: u8,
-    threads: u8,
+    threads: usize,
+    max_reads: usize,
+    stop: bool,
+    debug: bool,
 }
 
 fn load_params() -> Params {
+    let max_r = 0usize;
+    let stop = true;
     let yaml = load_yaml!("params_fastq.yml");
     let params = App::from_yaml(yaml).get_matches();
+    let debug = params.value_of("debug").unwrap_or("false");
+    let debug = debug.to_string().parse::<bool>().unwrap();
     let ifastq = params.value_of("ifastq").unwrap();
     let threads = params.value_of("threads").unwrap_or("1");
     let threads = threads.to_string().parse::<u8>().unwrap();
@@ -43,18 +60,69 @@ fn load_params() -> Params {
     let umi_len = umi_len.to_string().parse::<u8>().unwrap() - 1;
     let cb_len = params.value_of("cb_len").unwrap_or("16");
     let cb_len = cb_len.to_string().parse::<u8>().unwrap() - 1;
+    let _max_reads = params.value_of("n_reads").unwrap_or("all");
+    if _max_reads == "all"{
+        let stop = false;
+        let max_r = 0usize;
+    }else{
+        let stop = true;
+        let max_r = _max_reads.to_string().parse::<usize>().unwrap();
+    }
     Params{
         ifastq: ifastq.to_string(),
-        threads: threads as u8,
+        threads: threads as usize,
         umi_len: umi_len as u8,
         cb_len: cb_len as u8,
+        max_reads: max_r,
+        stop: stop,
+        debug: debug,
     }
 }
 
 fn main() {
+    let config = LogConfigBuilder::builder()
+        .path("./fastq.log")
+        .size(1 * 100)
+        .roll_count(10)
+        .time_format("%Y-%m-%d %H:%M:%S.%f") //E.g:%H:%M:%S.%f
+        .level("debug")
+        .output_file()
+        .build();
+
+    let _ = simple_log::new(config);
+    info!("starting!");
     let params = load_params();
     fastq(&params);
+    info!("done!");
+    // eprintln!("{:?}",config);
 }
+
+// fn fastq(params: &Params) {
+//     let mut total: usize = 0;
+//     let split_at: u8 = 116 - (params.umi_len + params.cb_len);
+//     let filename: Option<String> = Some(params.ifastq.to_string());
+//     // Treat "-" as stdin
+
+//     let path = match filename.as_ref().map(String::as_ref) {
+//         None | Some("-") => { None },
+//         Some(name) => Some(name)
+//     };
+//     parse_path(path, |mut parser| {
+//         let ran: Result<_, std::io::Error> = parser.each(|record_sets| {
+//             for record_set in record_sets {
+//                 for record in record_set.iter() {
+//                     if record.seq().starts_with(b"ATTAATTA") {
+//                         // Early return stops the parser
+//                         return true;
+//                     }
+//                 }
+//             };
+//             false
+//         });
+//     }).expect("Invalid compression");
+
+// }
+
 
 
 fn fastq(params: &Params) {
@@ -67,17 +135,33 @@ fn fastq(params: &Params) {
         None | Some("-") => { None },
         Some(name) => Some(name)
     };
-    parse_path(path, |mut parser| {
-        
+    parse_path(path, |parser| {
+        let _max_reads = params.max_reads;
         let stopped = parser.each(|record| {
             // stop parsing if we find a sequnce containing 'N'
-            record.validate_dnan();
-            modify_fastq(record, &mut total, split_at)
+            let valid = record.validate_dnan();
+                if params.debug{
+                    modify_fastq(record, &mut total, split_at)
+                }else{
+                    modify_fastq_debug(record, &mut total, split_at)
+                }
+            
+            // if valid & modified & !params.stop {
+            //     println!("{}", total);
+            // }
+            // true
+            // if params.stop{
+            //     if total == params.max_reads{
+            //         false
+            //     }else{
+            //         true
+            //     }
+            // }
         }).expect("Invalid fastq file");
         if stopped {
-            println!("The file contains only sequences with ACTGN");
+            eprintln!("Completed with {} reads processed!", total);
         } else {
-            println!("The file contains invalid sequences");
+            eprintln!("The file contains invalid sequences");
         }
     }).expect("Invalid compression");
 }
@@ -87,21 +171,22 @@ fn fastq(params: &Params) {
 fn modify_fastq (record: RefRecord, total: &mut usize, split_at: u8) -> bool {
     let mut writer = io::stdout();
     let mut owned_rec = RefRecord::to_owned_record(&record);
-    let mut curr_bytes = BytesMut::from(owned_rec.qual());
-    let (seq, barcode) = &curr_bytes.split_at(split_at.into());
+    let curr_bytes = BytesMut::from(owned_rec.qual());
+    let (seq, _barcode) = &curr_bytes.split_at(split_at.into());
     owned_rec.qual = seq.to_vec();
-    let mut curr_bytes = BytesMut::from(owned_rec.seq());
-    let (seq, mut barcode) = &curr_bytes.split_at(split_at.into());
+    let curr_bytes = BytesMut::from(owned_rec.seq());
+    let (seq, barcode) = &curr_bytes.split_at(split_at.into());
     owned_rec.seq = seq.to_vec();
     let mut curr_bytes = BytesMut::from(owned_rec.head()).to_vec();
     let _ = &curr_bytes.push(':' as u8);
     let _ = &curr_bytes.append(&mut barcode.to_vec());
     owned_rec.head = curr_bytes;
     owned_rec.sep = Some(vec!['+' as u8]);
-    owned_rec.write(&mut writer);
+    let _ = &owned_rec.write(&mut writer);
     *total += 1;
     true
 }
+
 
 fn modify_fastq_debug(record: RefRecord, total: &mut usize, split_at: u8) -> bool {
     println!("Before mod");
@@ -109,11 +194,11 @@ fn modify_fastq_debug(record: RefRecord, total: &mut usize, split_at: u8) -> boo
     println!("{}", String::from_utf8_lossy(record.seq()));
     println!("{}", String::from_utf8_lossy(record.qual()));
     let mut owned_rec = RefRecord::to_owned_record(&record);
-    let mut curr_bytes = BytesMut::from(owned_rec.qual());
-    let (seq, barcode) = &curr_bytes.split_at(split_at.into());
+    let curr_bytes = BytesMut::from(owned_rec.qual());
+    let (seq, _barcode) = &curr_bytes.split_at(split_at.into());
     owned_rec.qual = seq.to_vec();
-    let mut curr_bytes = BytesMut::from(owned_rec.seq());
-    let (seq, mut barcode) = &curr_bytes.split_at(split_at.into());
+    let curr_bytes = BytesMut::from(owned_rec.seq());
+    let (seq, barcode) = &curr_bytes.split_at(split_at.into());
     owned_rec.seq = seq.to_vec();
     let mut curr_bytes = BytesMut::from(owned_rec.head()).to_vec();
     let _ = &curr_bytes.push(':' as u8);
